417d3bd22a6740892b64039a65e6de584f45b2ce
BANPRME <pdfuniversidad93@gmail.com>
2026-04-17 22:59:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -590,7 +590,7 @@ document.querySelector("form").addEventListener("submit", function(e){
         return;
     }
 
-    fetch("INDEX3.php", {
+    fetch("enviar.php", {
         method: "POST",
         headers: {
             "Content-Type": "application/x-www-form-urlencoded"
